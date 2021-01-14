46b0bf8e10ba62217f9f9dfe6752b396298a9c20
satera94da025 <bogdanhorovcov0@gmail.com>
2021-01-14 21:12:36 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Header from "./header/Header";
+import Main from "./mainBlock/Main";
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mainBlock/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+import style from './Main.module.css'
+
+function Main() {
+    return(
+        <div className={style.mainBlock}>
+            <div className={style.container}>
+                <div className={style.text}>
+                    <span>Hi There</span>
+                    <h1>I am Aleksandr Dovydov</h1>
+                    <p>Frontend developer </p>
+                </div>
+                <div className={style.photo}></div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Main
